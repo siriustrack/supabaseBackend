8d6629236e0cd7f859a2802fe5ad1ea9c541c5ac
paulotfchaves <43656788+paulotfchaves@users.noreply.github.com>
2024-10-22 16:40:57 -0300

    refact: redisclient

--- src/config/redisClient.ts
+++ src/config/redisClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "redis";
 
-const redisClient = createClient({
+export const redisClient = createClient({
   url: process.env.REDIS_URL || "redis://localhost:6379",
 });
 
